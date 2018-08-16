2eb2e0cd90293d72f96edea0c32cc21dafbcc54d
fl3xc0d3r <fl3xc0d3r@gmail.com>
2018-08-15 23:33:33 -0600

    App 1: hello (you pythonic) world

---
+++ hello/program.py
@@ -0,0 +1,7 @@
+# ASk user name and print a greeting
+print('-------------------------------------')
+print('              HELLO APP              ')
+print('-------------------------------------')
+
+name = input('What is your name? : ')
+print("Hello there %s, glad to meet you" % name)
